3bc6cd44f61ab5f31437fec5deb15f49a7544a54
Allen Masud <100032855+allenmasud12@users.noreply.github.com>
2024-07-01 18:47:09 +0600

    firebase and release key

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:vendor_app/views/screen/home_page.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
